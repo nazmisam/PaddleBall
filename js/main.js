38cb03a9599b14b4b9400f677310e3da8f10bfc0
nazmisam <m.nazmi124@gmail.com>
2024-02-04 22:46:26 +0800

    Refactor paddle

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var canvas, canvasContext;
+
+window.onload = function () {
+  canvas = document.getElementById('gameCanvas');
+  if (canvas.getContext) {
+    var ctx = canvas.getContext('2d');
+
+    paddlesDraw(ctx, canvas)
+
+  }
+
+}
+
---
+++ js/paddle.js
@@ -0,0 +1,17 @@
+var paddle1Y = 100, paddle2Y = 100
+const PADDLE_DIST_FROM_EDGE = 25;
+const PADDLE_HEIGHT = 250;
+const PADDLE_WIDTH = 25;
+// const PADDLE_THICKNESS = BALL_SPEED_MAX + 2;
+
+const PADDLE_COMPUTER_MOVE_SPEED = 9.0;
+
+// const PADDLE_TWO_PLAYER_KEYBOARD_MOVE_SPEED = 18.0;
+
+
+function paddlesDraw(ctx) {
+  ctx.fillRect(PADDLE_DIST_FROM_EDGE, PADDLE_HEIGHT, PADDLE_WIDTH, paddle1Y);
+
+  ctx.fillRect(canvas.width - PADDLE_WIDTH - PADDLE_DIST_FROM_EDGE, PADDLE_HEIGHT, PADDLE_WIDTH, paddle2Y);
+
+}
